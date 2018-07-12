fd06736147f50c40cec661886af8c8591ee85e93
TinaGVI <eleono111@list.ru>
2018-07-12 21:26:16 +0300

    4.1. Глупый бот. [#31323]

---
+++ chapter_001/src/test/java/ru/job4j/condition/DummyBotTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.condition;
+
+/**
+ * @author TinaGVI 
+ * @version $Id$
+ * @since 0.1
+ */
+public class DummyBot {
+    /**
+     * Отвечает на вопросы.
+     * @param question Вопрос от клиента.
+     * @return Ответ.
+     */
+    public String answer(String question) {
+        String rsl = "Это ставит меня в тупик. Спросите другой вопрос.";
+        if ("Привет, Бот.".equals(question)) {
+            // заменить ... на правильный ответ rsl = "ответ по заданию".
+            rsl = ...;                    
+        } else if (....) { // заменить ... на проверку, что этот вопрос известен боту и он знает как на него ответить.
+            // заменить ... на правильный ответ rsl = "ответ по заданию".
+            rsl = ..;
+        }
+        return rsl;
+     }
+}
